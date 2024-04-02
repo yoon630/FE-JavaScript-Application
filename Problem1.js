29c11b36fe91220e99e69ef316304d3a9779c03c
kim nahyeon <stome6322@inha.edu>
2024-04-03 01:55:32 +0900

    Create Problem1.js

---
+++ Problem1.js
@@ -0,0 +1,13 @@
+// 1번 문제
+
+let Name = "주용";
+
+// ? 부분에 예측 값을 작성해주세요(코드 실행 금지!!!)
+
+alert(`hello ${1}`); // ?
+
+alert(`hello ${"Name"}`); // ?
+
+alert("hello ${Name}"); // ?
+
+alert(`hello ${Name}`); // ?
