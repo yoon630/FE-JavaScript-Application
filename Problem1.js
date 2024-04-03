2b1d90ee3b7118058fb5b7dc889e1c36e222b9d7
yoon630 <choiyunseon630@gmail.com>
2024-04-03 19:32:44 +0900

    problem1 commit

--- Problem1.js
+++ Problem1.js
@@ -4,10 +4,10 @@ let Name = "주용";
 
 // ? 부분에 예측 값을 작성해주세요(코드 실행 금지!!!)
 
-alert(`hello ${1}`); // ?
+alert(`hello ${1}`); // hello 1
 
-alert(`hello ${"Name"}`); // ?
+alert(`hello ${"Name"}`); // hello "Name"
 
-alert("hello ${Name}"); // ?
+alert("hello ${Name}"); // hello ${Name}
 
-alert(`hello ${Name}`); // ?
+alert(`hello ${Name}`); // hello 주용
