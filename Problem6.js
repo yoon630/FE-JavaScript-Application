39497176036f35ef222f148fcb123bb48f0cbf1e
yoon630 <choiyunseon630@gmail.com>
2024-04-03 23:59:00 +0900

    problem6 commit

--- Problem6.js
+++ Problem6.js
@@ -1,6 +1,20 @@
 // 6번 문제
 
-const arr = [{ part: "fe", name: "홍현", age: 26},
-  { part: "fe", name: "애리", age: 24},
-  { part: "fe", name: "나연", age: 23},
-  { part: "staff", name: "나현", age: 24}];
+let arr = [
+  { part: "fe", name: "홍현", age: 26 },
+  { part: "fe", name: "애리", age: 24 },
+  { part: "fe", name: "나연", age: 23 },
+  { part: "staff", name: "나현", age: 24 },
+];
+
+//6-1번
+newArr = arr.filter((value) => value.part != "staff");
+console.log(newArr);
+
+//6-2번
+newArr.sort((a, b) => b.age - a.age);
+console.log(newArr);
+
+//6-3번
+newArr = newArr.part.toUpperCase();
+console.log(newArr);
